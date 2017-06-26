b7a7d1db278257bf5f95016a87a5dd9afa1bc6cd
andrewhamili <rockr1521@gmail.com>
2017-06-26 15:45:08 +0800

    Fix almost done

--- JPCS Registration/Login.cs
+++ JPCS Registration/Login.cs
@@ -9,6 +9,7 @@ using Telerik.WinControls;
 using MySql.Data.MySqlClient;
 using Telerik.WinControls.UI;
 using System.Runtime.InteropServices;
+using System.Data.SqlClient;
 
 namespace JPCS_Registration
 {
@@ -47,13 +48,24 @@ namespace JPCS_Registration
             }
 
         }
+        void dbStatus()
+        {
+            Boolean stat = false;
+            stat = checkdbstat();
+            if (stat == false)
+            {
+
+                RadMessageBox.Show(this, "Unable to connect to MySQL database. Please fix your configuration.", "JPCS Registration", MessageBoxButtons.OK, RadMessageIcon.Info);
+                SettigsWindow();
+            }
+        }
         private void log_btn_login_Click(object sender, EventArgs e)
         {
             try
             {
                 conn = new MySqlConnection();
-                MySqlCommand command = gc.command;
-                conn.ConnectionString = connstring;
+
+                conn.ConnectionString = globalconfig.connstring;
                 MySqlDataReader reader = default(MySqlDataReader);
 
                 if (conn.State == ConnectionState.Open)
@@ -72,7 +84,7 @@ namespace JPCS_Registration
                         //query = "SELECT * FROM auth_accounts WHERE studno=@studno OR username=@username AND password=sha2(@password, 512);";
                         query = "CALL login(@1, @2);";
                         
-                        command = new MySqlCommand(query, conn);
+                        MySqlCommand command = new MySqlCommand(query, conn);
                         command.Parameters.AddWithValue("1", log_tb_username.Text);
                         command.Parameters.AddWithValue("2", log_tb_password.Text);
                         reader = command.ExecuteReader();
@@ -143,6 +155,9 @@ namespace JPCS_Registration
             if (check == false)
             {
                 RadMessageBox.Show(this, "Unable to connect to MySQL database. Please fix your configuration.", "JPCS Registration", MessageBoxButtons.OK, RadMessageIcon.Info);
+                log_server_status.Text = "Offline";
+                log_server_status.ForeColor = Color.Red;
+                SettigsWindow();
             }
 
         }
@@ -174,7 +189,7 @@ namespace JPCS_Registration
             Boolean loopstopper = false;
             conn = new MySqlConnection();
             conn.ConnectionString = globalconfig.connstring;
-            MySqlCommand command = gc.command;
+
             try
             {
                 if (conn.State == ConnectionState.Open)
@@ -182,16 +197,15 @@ namespace JPCS_Registration
                     conn.Close();
                 }
                 constate = false;
-                loopstopper = true;
-                conn.ConnectionString = connstring;
+                loopstopper = false;
                 conn.Open();
                 constate = true;
+                loopstopper = true;
                 conn.Close();
             }
             catch (Exception ex)
             {
-                RadMessageBox.Show("Please correct your connection configuration", "JPCS Registration");
-                settings.ShowDialog();
+                loopstopper = false;
             }
             finally
             {
@@ -220,7 +234,7 @@ namespace JPCS_Registration
             }
             else
             {
-                checkdbstat();
+                dbStatus();
             }
             
         }
